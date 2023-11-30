6074f0a2ee984b863e9ebb61149e233d85ff7d53
Van Chi Hieu <vanchihieu3@gmail.com>
2023-11-30 21:29:05 +0700

    Navbar UI

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+import React from "react";
+
+const MenuItem: React.FC<MenuItemProps> = ({ label, onClick }) => {
+  return (
+    <div
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+      onClick={onClick}
+    >
+      {label}
+    </div>
+  );
+};
+
+export default MenuItem;
