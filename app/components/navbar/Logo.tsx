ce3c126b4b0df12f1252360d06f39ba3fbacf7c7
Van Chi Hieu <vanchihieu3@gmail.com>
2023-12-05 07:43:12 +0700

    categories UI

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -8,6 +8,7 @@ const Logo = () => {
   return (
     <div>
       <Image
+        onClick={() => router.push("/")}
         alt="Logo"
         className="hidden md:block cursor-pointer"
         height="100"
